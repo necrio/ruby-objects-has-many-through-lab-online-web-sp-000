1ea904423bd728f7529af5cd21257171d4cb712e
Keith Isham <kisham8899@gmail.com>
2019-05-22 14:42:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,60 @@
-require 'pry'
+# require 'pry'
+#
+# class Artist
+#
+#   attr_accessor :name, :artist, :genre
+#
+#   ALL=[]
+#
+#   def initialize(name)
+#     @name = name
+#     ALL << self
+#   end
+#
+#   def self.all
+#     ALL
+#   end
+#
+#   def new_song(name, genre)
+#     song = Song.new(name, self, genre)
+#     self.songs.last
+#   end
+#
+#   def songs
+#     Song.all.select do |song|
+#       song.artist == self
+#     end
+#   end
+#
+#   def genres
+#     Song.all.map do |song|
+#       song.genre
+#     end
+#   end
+#
+# end
+
 
 class Artist
+	attr_accessor :name, :songs
 
-  attr_accessor :name, :artist, :genre
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
 
-  ALL=[]
+	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
 
-  def initialize(name)
-    @name = name
-    ALL << self
-  end
+	def songs #used as self.songs in method below
+		@songs
+	end
 
-  def self.all
-    ALL
-  end
-
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    self.songs.last
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-
-  def genres
-    Song.all.map do |song|
-      song.genre
-    end
-  end
-
+	def genres #returns an array of genre objects
+		self.songs.collect do |song|
+			song.genre
+		end
+	end
 end
