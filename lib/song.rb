fb53e6d8c508fdec63c2df19215ed6cba38d75f4
Keith Isham <kisham8899@gmail.com>
2019-05-22 14:46:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,31 @@
 # require 'pry'
-# 
+#
 # class Song
-# 
+#
 #   attr_accessor :name, :artist, :genre
-# 
+#
 #   ALL = []
-# 
+#
 #   def initialize(name="", artist="", genre="")
 #     @name = name
 #     @artist = artist
 #     @genre = genre
 #     ALL << self
 #   end
-# 
+#
 #   def self.all
 #     ALL
 #   end
-# 
+#
 #   def artist
 #     @artist
 #   end
-# 
+#
 # end
 
 class Song
   attr_accessor :name, :artist, :genre
- 
+
   def initialize(name, genre)
     @name = name
     @genre = genre
