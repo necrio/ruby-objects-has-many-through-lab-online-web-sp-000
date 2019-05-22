429e8a6788378c4d5f8403bdf0aba25457185fbb
Keith Isham <kisham8899@gmail.com>
2019-05-22 14:43:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,60 +1,35 @@
-# require 'pry'
-#
-# class Artist
-#
-#   attr_accessor :name, :artist, :genre
-#
-#   ALL=[]
-#
-#   def initialize(name)
-#     @name = name
-#     ALL << self
-#   end
-#
-#   def self.all
-#     ALL
-#   end
-#
-#   def new_song(name, genre)
-#     song = Song.new(name, self, genre)
-#     self.songs.last
-#   end
-#
-#   def songs
-#     Song.all.select do |song|
-#       song.artist == self
-#     end
-#   end
-#
-#   def genres
-#     Song.all.map do |song|
-#       song.genre
-#     end
-#   end
-#
-# end
-
+require 'pry'
 
 class Artist
-	attr_accessor :name, :songs
 
-	def initialize(name)
-		@name = name
-		@songs = []
-	end
+  attr_accessor :name, :artist, :genre
 
-	def add_song(song)
-		@songs << song
-		song.artist = self
-	end
+  ALL=[]
 
-	def songs #used as self.songs in method below
-		@songs
-	end
+  def initialize(name)
+    @name = name
+    ALL << self
+  end
 
-	def genres #returns an array of genre objects
-		self.songs.collect do |song|
-			song.genre
-		end
-	end
+  def self.all
+    ALL
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
 end
