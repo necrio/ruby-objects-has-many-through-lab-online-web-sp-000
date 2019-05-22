196e12ebd1bd230a04351dca15da23df6471b32d
Keith Isham <kisham8899@gmail.com>
2019-05-22 14:46:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,56 +1,56 @@
 # require 'pry'
-# 
+#
 # class Artist
-# 
+#
 #   attr_accessor :name, :artist, :genre
-# 
+#
 #   ALL=[]
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     ALL << self
 #   end
-# 
+#
 #   def self.all
 #     ALL
 #   end
-# 
+#
 #   def new_song(name, genre)
 #     song = Song.new(name, self, genre)
 #     self.songs.last
 #   end
-# 
+#
 #   def songs
 #     Song.all.select do |song|
 #       song.artist == self
 #     end
 #   end
-# 
+#
 #   def genres
 #     Song.all.map do |song|
 #       song.genre
 #     end
 #   end
-# 
+#
 # end
 
 class Artist
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @songs = []
   end
- 
+
   def add_song(song)
     @songs << song
     song.artist = self
   end
- 
+
   def songs
     @songs
   end
- 
+
   def genres
     self.songs.collect do |song|
       song.genre
