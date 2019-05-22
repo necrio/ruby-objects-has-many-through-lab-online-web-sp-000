dbf8b75a1734d4e10d3915590689808d629b266b
Keith Isham <kisham8899@gmail.com>
2019-05-22 14:43:46 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,47 +1,24 @@
-# class Genre
-#   attr_accessor :name, :songs
-#
-#
-#
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-#
-#   def songs
-#     @songs
-#   end
-#
-#   def add_song(song)
-#     @songs << song
-#   end
-#
-#   def artists
-#     @songs.collect do |song|
-#       song.artist
-#     end
-#   end
-# end
-
 class Genre
-	attr_accessor :name, :songs
+  attr_accessor :name
 
-	def initialize(name)
-		@name = name
-		@songs = []
-	end
 
-	def songs
-		@songs
-	end
 
-	def add_song(song)
-		@songs << song
-	end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-	def artists #collect all the artists of a genre
-		@songs.collect do |song|
-			song.artist
-		end
-	end
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    @songs.collect do |song|
+      song.artist
+    end
+  end
 end
